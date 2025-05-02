cd03c96edca9d285f72198d6ab9f14f2a9f4dd78
Sean <icekit001@gmail.com>
2025-05-02 08:48:17 +0100

    file name correction

--- src/Components/InformationBar.jsx
+++ src/Components/InformationBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import '../styles/informationBar.css'
+import '../styles/InformationBar.css'
 const InformationBar = () => {
   return (
     <>
